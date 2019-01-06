7bf5b91ed8b6d4455a0b8807e8ef86c6602e2500
Baptiste Canton <bat@sbz.fr>
2019-01-06 16:35:26 +0100

    Add source files

---
+++ utmost.go
@@ -0,0 +1,100 @@
+package utmost
+
+import (
+	"sync"
+)
+
+const (
+	// DefaultUtmost is used if you ask a negative limit
+	DefaultUtmost = 100
+)
+
+// TicketsMachine is our base type
+type TicketsMachine struct {
+	dispenser chan int
+	limit     int
+	dispensed int
+	inUse     int
+	maxInUse  int
+	wg        sync.WaitGroup
+	m         sync.RWMutex
+}
+
+// Wait blocks until the WaitGroup counter is zero.
+func (t *TicketsMachine) Wait() {
+	t.wg.Wait()
+}
+
+// New returns a initialized *TicketsMachine
+func New(limit int) *TicketsMachine {
+	// "int" is enough ((limitint=int(^uint(0) >> 1)))
+	if limit < 1 {
+		limit = DefaultUtmost
+	}
+	t := &TicketsMachine{
+		dispenser: make(chan int, limit),
+		limit:     limit,
+	}
+	for i := 0; i < limit; i++ {
+		t.dispenser <- i
+	}
+	return t
+}
+
+// Go executes your function as a goroutine, and blocks awaiting a free ticket if needed
+func (t *TicketsMachine) Go(goroutine func()) {
+	ticket := <-t.dispenser
+	t.m.Lock()
+	t.inUse++
+	t.dispensed++
+	if t.maxInUse < t.inUse {
+		t.maxInUse = t.inUse
+	}
+	t.m.Unlock()
+	t.wg.Add(1)
+	go func() {
+		defer func() {
+			// inUse must be decremented before giving our ticket back to avoid
+			// this racy example:
+			// (routine A) t.dispenser <- ticket
+			// (routine B) ticket := <-t.dispenser
+			// (routine B) Lock ... inUse++ ... Unlock
+			// < here, InUse() (and so MaxInUse() too) = limit+1 ! >
+			// (routine A) Lock ... inUse--
+			t.m.Lock()
+			t.inUse--
+			t.m.Unlock()
+			t.dispenser <- ticket
+			t.wg.Done()
+		}()
+		goroutine()
+	}()
+}
+
+// Limit returns the limit of simultaneous dispensed tickets
+func (t *TicketsMachine) Limit() int {
+	t.m.RLock()
+	defer t.m.RUnlock()
+	return t.limit
+}
+
+// Dispensed returns the total number of dispensed tickets
+func (t *TicketsMachine) Dispensed() int {
+	t.m.RLock()
+	defer t.m.RUnlock()
+	return t.dispensed
+}
+
+// MaxInUse returns the max simultaneous used tickets
+func (t *TicketsMachine) MaxInUse() int {
+	t.m.RLock()
+	defer t.m.RUnlock()
+	return t.maxInUse
+}
+
+// InUse returns the currently used tickets
+func (t *TicketsMachine) InUse() int {
+	t.m.RLock()
+	defer t.m.RUnlock()
+	return t.inUse
+}
---
+++ utmost_test.go
@@ -0,0 +1,80 @@
+package utmost_test
+
+import (
+	"math/rand"
+	"testing"
+	"time"
+
+	utmost "github.com/batmac/go-utmost"
+)
+
+func TestUtmostBasic(t *testing.T) {
+	limit := 5
+	tm := utmost.New(limit)
+	if tm.Limit() != limit {
+		t.Fail()
+	}
+	if tm.InUse() != 0 {
+		t.Fail()
+	}
+	if tm.MaxInUse() != 0 {
+		t.Fail()
+	}
+	if tm.Dispensed() != 0 {
+		t.Fail()
+	}
+	tm.Wait()
+
+}
+func TestUtmostNegative(t *testing.T) {
+	tm := utmost.New(-1)
+	if tm.Limit() != utmost.DefaultUtmost {
+		t.Fail()
+	}
+	tm.Wait()
+}
+
+func launchDummy(tb testing.TB, nbTotal, nbLimit, maxTime int) *utmost.TicketsMachine {
+	tb.Logf("launchDummy: nbTotal=%d, nbLimit=%d, maxTime=%d", nbTotal, nbLimit, maxTime)
+	tm := utmost.New(nbLimit)
+	if nbTotal < 1 || nbLimit < 1 || maxTime < 1 {
+		return tm
+	}
+	for i := 0; i < nbTotal; i++ {
+		tm.Go(func() {
+			time.Sleep(time.Duration(rand.Intn(maxTime)) * time.Millisecond)
+		})
+	}
+	if tm.Dispensed() != nbTotal {
+		tb.Fatal("Dispensed:", tm.Dispensed(), "!=", nbTotal)
+	}
+	if tm.MaxInUse() > nbLimit {
+		tb.Fatal("MaxInUse:", tm.MaxInUse(), ">", nbLimit)
+	}
+	// fmt.Println(tm.MaxInUse())
+	// tm.Wait()
+	return tm
+}
+
+func TestUtmostMany(t *testing.T) {
+	_ = launchDummy(t, rand.Intn(8000), rand.Intn(8000), rand.Intn(1000))
+}
+
+func TestUtmostManyManyTimes(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping test in short mode.")
+	}
+	loop := 10
+	for i := 0; i < loop; i++ {
+		TestUtmostMany(t)
+	}
+}
+
+func BenchmarkUtmost(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		tm := launchDummy(b, b.N, b.N/2, 10)
+		b.StopTimer()
+		tm.Wait()
+		b.StartTimer()
+	}
+}
